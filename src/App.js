2a3bcb66ee0955ef7d0b915c3add8711e01a88ea
AmirRzp <rezarayan78@gmail.com>
2022-09-18 14:14:44 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        const { value, onIncreament, onDecreament } = this.props
+        return (
+            <main>
+                <div className='counter'>
+                    <h1>Counter : {value}</h1>
+                </div>
+                <div className='btn-container'>
+                    <button onClick={onIncreament} className='add'>Increament</button>
+                    <button onClick={onDecreament} className='sub'>Decreament</button>
+                </div>
+
+            </main>
+        )
+    }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from 'react-dom'
+import App from './App'
+import { createStore } from 'redux'
+import reducer from "./reducers/reducer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const store = createStore(reducer);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const rendering = () => ReactDom.render(<App
+    value={store.getState()}
+    onIncreament={() => store.dispatch({ type: 'INCREAMENT' })}
+    onDecreament={() => store.dispatch({ type: 'DECREAMENT' })}
+/>, document.getElementById('root'))
+
+rendering();
+
+store.subscribe(rendering)
